51945343b2739b8446522d07c02044ed14840d61
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-05-09 09:49:49 +0600

    update quantity input

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Table,
   TableBody,
@@ -5,12 +6,18 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { FaArrowsRotate, FaClosedCaptioning } from "react-icons/fa6";
 
 const Checkout = () => {
+  const [quantity, setQuantity] = useState(1);
   const rows = [
     { name: "Walton AC", price: 120000, image: "Banner", quantity: 1 },
   ];
+  const handleQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+  console.log(quantity);
   return (
     <div className="grid grid-cols-3 gap-4 justify-between">
       <div className="col-span-2 py-4">
@@ -19,6 +26,7 @@ const Checkout = () => {
             <TableRow>
               <TableCell>Image</TableCell>
               <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Model</TableCell>
               <TableCell align="right">Quantity</TableCell>
               <TableCell align="right">Update & Delete</TableCell>
               <TableCell align="right">Price</TableCell>
@@ -29,7 +37,20 @@ const Checkout = () => {
               <TableRow key={row.name}>
                 <TableCell>{row.image}</TableCell>
                 <TableCell align="right">{row.name}</TableCell>
-                <TableCell align="right">{row.quantity}</TableCell>
+                <TableCell align="right">Ashaduzzaman Sojib</TableCell>
+                <TableCell className="flex gap-2">
+                  <input
+                    onChange={(e) => handleQuantity(e)}
+                    type="number"
+                    min={1}
+                    placeholder={row.quantity}
+                    className="w-[80px]"
+                  />
+                  <button className="px-3 bg-primary text-white">
+                    <FaArrowsRotate />
+                  </button>
+                  <button className="px-3 bg-accent text-white">x</button>
+                </TableCell>
                 <TableCell align="right">
                   <button>Update</button>
                 </TableCell>
